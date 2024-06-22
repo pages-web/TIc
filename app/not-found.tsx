cc207c5e512d056313dbb2a63ac7f7fc0f88255f
KhBaterdene <dev@erxes.io>
2024-06-22 21:06:39 +0800

    sync

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="h-[40vw] flex items-center justify-center flex-col">
+    <div className="flex-auto flex items-center justify-center flex-col">
       <h2 className="text-4xl font-semibold">404</h2>
       <p className="text-neutral-500 pt-2 pb-1">Энэ хуудас олдсонгүй</p>
       <Button asChild variant="link">
